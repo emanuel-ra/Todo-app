d849e204fa85f121a5b56c537fa5a94310936ae4
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-03-26 16:56:09 -0600

    ✨ Feat(css) add transition on banner

--- app/src/Components/Banner.tsx
+++ app/src/Components/Banner.tsx
@@ -4,12 +4,11 @@ function Banner() {
     return (
       <div
         className={`w-full h-[200px] md:h-[300px] 
+          transition delay-300 ease-in-out
           bg-mobile-light dark:bg-mobile-dark bg-no-repeat md:bg-desktop-light dark:md:bg-desktop-dark
-          flex justify-center items-center
-          transition
+          flex justify-center items-center          
         `}
-      >       
-      </div>
+      ></div>
     );
 
 }
