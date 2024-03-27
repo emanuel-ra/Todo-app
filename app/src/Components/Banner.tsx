0a9c2f08652991ea6d49ce5556cdcd94a91500f2
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2024-03-26 23:31:39 -0600

    NONE

--- app/src/Components/Banner.tsx
+++ app/src/Components/Banner.tsx
@@ -1,16 +1,13 @@
 function Banner() {
-    
-
-    return (
-      <div
-        className={`w-full h-[200px] md:h-[300px] 
+  return (
+    <div
+      className={`w-full h-[200px] md:h-[300px] 
           transition delay-300 ease-in-out
           bg-mobile-light dark:bg-mobile-dark bg-no-repeat md:bg-desktop-light dark:md:bg-desktop-dark
           flex justify-center items-center          
         `}
-      ></div>
-    );
-
+    ></div>
+  );
 }
 
-export default Banner
+export default Banner;
